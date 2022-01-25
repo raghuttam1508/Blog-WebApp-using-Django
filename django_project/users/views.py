cf3e5a0afd3a1c00fe79ddcf49d8dfffb4daecf8
Raghuttam Parvatikar <raghu.parvatikar@gmail.com>
2022-01-25 12:12:50 +0530

    user creation

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -7,9 +7,10 @@ def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get("username")
             messages.success(request, f"Account created for {username}!")
             return redirect("blog-home")
-        else:
-            form = UserCreationForm()
+    else:
+        form = UserCreationForm()
     return render(request, "users/register.html", {"form": form})
