da1d3261acbd23950ce15b259ebc1ad40195aec4
raghuttam1508 <raghu.parvatikar@gmail.com>
2022-01-27 17:10:07 +0530

    adding email in fields

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -5,7 +5,8 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    name = forms.CharField()
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ["name", "username", "email", "password1", "password2"]
